aac5bab430cc666dc73ac6c70236c6df3a5e75b1
David Hellinga <d.hellinga@student.fontys.nl>
2022-03-31 13:22:55 +0200

    Added test project

---
+++ forgedinthelore-net-test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace forgedinthelore_net_test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
